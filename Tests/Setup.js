7b5b3b6f57d8729624425393371f121c3e3b500e
jpklein <jpklein@gmail.com>
2017-07-04 04:11:09 -0700

    Moves test workaround to unified setup file

--- Tests/Setup.js
+++ Tests/Setup.js
@@ -2,3 +2,16 @@ jest
 .mock('react-native-device-info', () => {
   return { isTablet: jest.fn(() => { return false }) }
 })
+// @see https://github.com/facebook/jest/issues/3707
+.mock('TextInput', () => {
+  const RealComponent = require.requireActual('TextInput')
+  const React = require('React')
+  class TextInput extends React.Component {
+    render() {
+      delete this.props.autoFocus;
+      return React.createElement('TextInput', this.props, this.props.children)
+    }
+  }
+  TextInput.propTypes = RealComponent.propTypes
+  return TextInput
+})
