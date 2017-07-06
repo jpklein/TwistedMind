cde44a404a250a6875812fd91b00c65cd87ed0f3
jpklein <jpklein@gmail.com>
2017-07-05 22:02:40 -0700

    Attempts testing login saga

--- test/sagas/login.test.js
+++ test/sagas/login.test.js
@@ -1,8 +1,80 @@
 import GameSparks from 'gamesparks-sdk';
+import CryptoJS from 'crypto-js';
+import { call } from 'redux-saga/effects';
 
-it('has GameSparks object', () => {
-  const sdk = new GameSparks();
+const sdk = new GameSparks();
+
+// it('has the GameSparks sdk', () => {
+//   expect(() => {
+//     sdk.send('', (e) => { throw(e.error); });
+//   }).toThrow(/NOT_INITIALISED/);
+// });
+
+// it('initializes the sdk against preview servers', () => {
+//   expect(() => {
+//     const onNonce = (n) => {
+//       throw(n);
+//       return CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA256(n, 'bv7XLbgfeKWviKsfw4Uu2rUc64ncn61S'));
+//     };
+//     const onInit = () => {
+//       throw("Initialized");
+//     };
+//     const onMessage = (m) => {
+//       throw(JSON.stringify(m));
+//     };
+//     sdk.initPreview({
+//       key: 'h313710gdMs0',
+//       onNonce: onNonce,
+//       onInit: onInit,
+//       onMessage: onMessage
+//     });
+//   }).toThrow(/YO/);
+// });
+
+// function* initSdk() {
+//   const onNonce = (n) => {
+//     throw(n);
+//     return CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA256(n, 'bv7XLbgfeKWviKsfw4Uu2rUc64ncn61S'));
+//   };
+//   const onInit = () => {
+//     throw("Initialized");
+//   };
+//   const onMessage = (m) => {
+//     throw(JSON.stringify(m));
+//   };
+//   yield call(sdk.initPreview, {
+//     key: 'h313710gdMs0',
+//     onNonce: onNonce,
+//     onInit: onInit,
+//     onMessage: onMessage
+//   });
+// }
+//
+// it('initializes the sdk against preview servers', () => {
+//   expect(() => {
+//     const gen = initSdk();
+//     gen.next().done;
+//   }).toThrow(/YO/);
+// });
+
+it('initializes the sdk against preview servers', () => {
   expect(() => {
-    sdk.send('', (e) => { throw(e.error); });
-  }).toThrow(/NOT_INITIALISED/);
+    const onNonce = (n) => {
+      throw(JSON.stringify(n));
+      return CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA256(n, 'bv7XLbgfeKWviKsfw4Uu2rUc64ncn61S'));
+    };
+    const onInit = () => {
+      throw("Initialized");
+    };
+    const onMessage = (m) => {
+      throw(JSON.stringify(m));
+    };
+    const gen = call(sdk.initPreview, {
+      key: 'h313710gdMs0',
+      onNonce: onNonce,
+      onInit: onInit,
+      onMessage: onMessage
+    });
+    gen.next();
+  }).toThrow(/SOMETHING/);
 });
