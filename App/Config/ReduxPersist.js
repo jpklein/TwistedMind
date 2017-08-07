a3e5dd1c1748f86017aa21d0634dedde1bdf7045
jpklein <jpklein@gmail.com>
2017-08-06 21:09:32 -0700

    Adds redux-persist middleware to reset socket state on rehydrate

--- App/Config/ReduxPersist.js
+++ App/Config/ReduxPersist.js
@@ -1,5 +1,25 @@
 import immutablePersistenceTransform from '../Services/ImmutablePersistenceTransform'
 import { AsyncStorage } from 'react-native'
+import { INITIAL_STATE as SDK_DEFAULT } from '../Redux/GamesparksRedux.js'
+import { INITIAL_STATE as USER_DEFAULT } from '../Redux/LoginRedux.js'
+
+const ignoreGamesparksTransmitState = {
+  in: raw => raw,
+  out: state => {
+    if ('environment' in state) {
+      // only rehydrates endpoint information
+      state.initializing = SDK_DEFAULT.initializing
+      state.connected = SDK_DEFAULT.connected
+    } else if ('username' in state) {
+      // rehydrates user profile information
+      state.error = USER_DEFAULT.error
+      state.fetching = USER_DEFAULT.fetching
+      // @todo forces login after rehydrate?
+      // state.authToken = USER_DEFAULT.authToken
+    }
+    return state
+  }
+}
 
 // More info here:  https://shift.infinite.red/shipping-persistant-reducers-7341691232b1
 const REDUX_PERSIST = {
@@ -7,10 +27,11 @@ const REDUX_PERSIST = {
   reducerVersion: '1.0',
   storeConfig: {
     storage: AsyncStorage,
-    blacklist: ['search', 'navigation', 'appState'], // reducer keys that you do NOT want stored to persistence here
-    // whitelist: [], Optionally, just specify the keys you DO want stored to
-    // persistence. An empty array means 'don't store any reducers' -> infinitered/ignite#409
-    transforms: [immutablePersistenceTransform]
+    blacklist: ['search', 'navigation', 'appState'],
+    transforms: [
+      immutablePersistenceTransform,
+      ignoreGamesparksTransmitState
+    ]
   }
 }
 
