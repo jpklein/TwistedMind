a089caf438f76d60391c1a1c5a2f60ee0aff64f1
jpklein <jpklein@gmail.com>
2017-10-19 19:09:41 -0700

    Adds button to home screen. Resolves #8

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 import styles from '../Containers/Styles/DefaultScreenStyle'
 
 class HomeScreen extends React.Component {
   static navigationOptions = { title: 'Home' }
   render () {
-    // const { goBack } = this.props.navigation
+    const { navigate } = this.props.navigation
     return (
       <View style={styles.container}>
         <Text style={styles.headerText}>Love anagrams?</Text>
         <Text style={styles.headerText}>Then you'll love</Text>
         <Text style={styles.headerText}>Twisted Mind</Text>
+        <Button
+          onPress={() => navigate('PlayScreen')}
+          title='Continue last game'
+        />
       </View>
     )
   }
