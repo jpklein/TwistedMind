3062fa08fd4af034b3db930d9e3f4c075d878b5c
jpklein <jpklein@gmail.com>
2017-10-21 22:37:12 -0700

    Embeds image in text. Resolves #11
    - NB wrapping text around a floating image is not supported: https://stackoverflow.com/questions/38508429/wrapping-text-around-an-image-with-react-native

--- App/Containers/LearnScreen.js
+++ App/Containers/LearnScreen.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { ScrollView, StyleSheet, Text } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import styles from '../Containers/Styles/DefaultScreenStyle'
+import { Images } from '../Themes'
 
 class LearnScreen extends React.Component {
   render () {
     return (
       <ScrollView style={styles.container}>
-        <Text style={[css.txt, css.br]}>Each game includes 10 clues for you to solve. Each clue is a sentence that contains one or more consecutive ‘keywords’. We don’t tell you which words are the keywords, but we do tell you how many there are. By re-arranging all the letters in the keyword(s), you can make a <Text style={css.em}>single-word</Text> anagram that solves the clue.</Text>
+        <View style={css.flex}>
+          <Image style={css.img} source={Images.lighthead} />
+          <Text style={[css.flex, css.txt, css.br]}>Each game includes 10 clues for you to solve. Each clue is a sentence that contains one or more consecutive ‘keywords’. We don’t tell you which words are the keywords, but we do tell you how many there are. By re-arranging all the letters in the keyword(s), you can make a <Text style={css.em}>single-word</Text> anagram that solves the clue.</Text>
+        </View>
         <Text style={css.txt}>Here’s a simple clue with a single keyword:</Text>
         <Text style={css.clue}>The cast of the play includes several felines.</Text>
         <Text style={[css.txt, css.br]}>The answer is ‘cats’, which is an anagram of the keyword ‘cast’; and of course, 'cats' are 'felines'.</Text>
@@ -22,6 +26,18 @@ class LearnScreen extends React.Component {
 }
 
 const css = StyleSheet.create({
+  flex: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 5
+  },
+  img: {
+    width: 70,
+    height: 107,
+    marginRight: 20,
+    marginTop: 5,
+    resizeMode: 'contain'
+  },
   txt: {
     fontSize: 18,
     // fontFamily: 'arial narrow',
