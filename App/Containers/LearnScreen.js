94a8772af39acc4a531249254721c98c0b24440b
jpklein <jpklein@gmail.com>
2017-10-21 22:25:20 -0700

    Cleans up paragraph styling

--- App/Containers/LearnScreen.js
+++ App/Containers/LearnScreen.js
@@ -6,16 +6,16 @@ class LearnScreen extends React.Component {
   render () {
     return (
       <ScrollView style={styles.container}>
-        <Text style={[css.txt, css.just]}>Each game includes 10 clues for you to solve. Each clue is a sentence that contains one or more consecutive ‘keywords’. We don’t tell you which words are the keywords, but we do tell you how many there are. By re-arranging all the letters in the keyword(s), you can make a <Text style={{fontWeight: 'bold', fontStyle: 'italic'}}>single-word</Text> anagram that solves the clue.</Text>
-        <Text style={[css.txt, css.just, css.br]}>Here’s a simple clue with a single keyword:</Text>
-        <Text style={[css.clue, css.br]}>The cast of the play includes several felines.</Text>
-        <Text style={[css.txt, css.just, css.br]}>The answer is ‘cats’, which is an anagram of the keyword ‘cast’; and of course, 'cats' are 'felines'.</Text>
-        <Text style={[css.txt, css.just, css.br]}>Now here’s a clue that is a little harder. It contains two keywords:</Text>
-        <Text style={[css.clue, css.br]}>The rep said only good things about his products.</Text>
-        <Text style={[css.txt, css.just, css.br]}>In this case, the answer is ‘praised’ – an anagram of the keywords ‘rep said’. It's the correct answer, because 'praise' means 'to say good things about'. </Text>
-        <Text style={[css.txt, css.just, css.br]}>Note that keywords and answers may include proper names. For example:</Text>
-        <Text style={[css.clue, css.br]}>The book told a tale set in this north-western city.</Text>
-        <Text style={[css.txt, css.just, css.br]}>The answer – Seattle – is an anagram of ‘tale set’.</Text>
+        <Text style={[css.txt, css.br]}>Each game includes 10 clues for you to solve. Each clue is a sentence that contains one or more consecutive ‘keywords’. We don’t tell you which words are the keywords, but we do tell you how many there are. By re-arranging all the letters in the keyword(s), you can make a <Text style={css.em}>single-word</Text> anagram that solves the clue.</Text>
+        <Text style={css.txt}>Here’s a simple clue with a single keyword:</Text>
+        <Text style={css.clue}>The cast of the play includes several felines.</Text>
+        <Text style={[css.txt, css.br]}>The answer is ‘cats’, which is an anagram of the keyword ‘cast’; and of course, 'cats' are 'felines'.</Text>
+        <Text style={css.txt}>Now here’s a clue that is a little harder. It contains two keywords:</Text>
+        <Text style={css.clue}>The rep said only good things about his products.</Text>
+        <Text style={[css.txt, css.br]}>In this case, the answer is ‘praised’ – an anagram of the keywords ‘rep said’. It's the correct answer, because 'praise' means 'to say good things about'.</Text>
+        <Text style={css.txt}>Note that keywords and answers may include proper names. For example:</Text>
+        <Text style={css.clue}>The book told a tale set in this north-western city.</Text>
+        <Text style={[css.txt, css.br]}>The answer – Seattle – is an anagram of ‘tale set’.</Text>
       </ScrollView>
     )
   }
@@ -25,17 +25,18 @@ const css = StyleSheet.create({
   txt: {
     fontSize: 18,
     // fontFamily: 'arial narrow',
-    lineHeight: 22
-  },
-  just: {
-    paddingTop: 10,
+    lineHeight: 22,
     textAlign: 'justify' // justify not supported on android
+  },
+  em: {
+    fontStyle: 'italic',
+    fontWeight: 'bold'
   },
   clue: {
     fontStyle: 'italic',
     fontWeight: 'bold',
     paddingLeft: 18,
-    lineHeight: 28,
+    paddingVertical: 10,
     color: 'blue'
   },
   br: {
