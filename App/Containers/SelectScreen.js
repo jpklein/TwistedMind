1c464193b7124e056cf42f7b28b5736a25d189e7
jpklein <jpklein@gmail.com>
2017-10-23 16:03:16 -0700

    Renders game data in card

--- App/Containers/SelectScreen.js
+++ App/Containers/SelectScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import styles from '../Containers/Styles/DefaultScreenStyle'
 
 class SelectScreen extends React.Component {
@@ -16,13 +16,48 @@ class SelectScreen extends React.Component {
   ]
 
   render () {
+    // @todo wraps cta in Button
     return (
       <ScrollView style={styles.container}>
-        <Text>{JSON.stringify(this.games[0])}</Text>
+        <View style={css.cards}>
+          {this.games.map(game => {
+            return (
+              <View key={game.id} style={css.card}>
+                <View style={css.txt}>
+                  <Text>{game.category}</Text>
+                  <Text style={styles.h1}>{game.label}</Text>
+                  <Text>{game.level}</Text>
+                </View>
+                <View style={css.status}>
+                  <Text style={css.cta}>{game.status}</Text>
+                  <Text>{game.played}</Text>
+                </View>
+              </View>
+            )
+          })}
+        </View>
       </ScrollView>
-    )
     )
   }
 }
+
+const css = StyleSheet.create({
+  cards: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  card: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
+    marginBottom: 5
+  },
+  img: null,
+  txt: {
+    flex: 1
+  },
+  cta: null
+})
 
 export default SelectScreen
